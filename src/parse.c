aac839fe15c4def3deb56a8b307058b9aac29159
ianertson <sebbekarlsson97@gmail.com>
2018-11-12 19:17:11 +0100

    parse_expr

--- src/parse.c
+++ src/parse.c
@@ -41,6 +41,50 @@ ast_node* parse_term(parse_state* state) {
     return node;
 }
 
-ast_node* parse_expr(parse_state* state) { return (void*)0; }
+ast_node* parse_expr(parse_state* state) {
+    token* tok = (void*)0;
+
+    // invalid pointer type
+    ast_node* node = parse_term(state);
+    
+    while(
+        state->current_token->type == _OP_PLUS ||
+        state->current_token->type == _OP_SUBTRACT ||
+        state->current_token->type == _NOT_EQUALS ||
+        state->current_token->type == _LESS_THAN ||
+        state->current_token->type == _LARGER_THAN ||
+        state->current_token->type == _LESS_OR_EQUALS ||
+        state->current_token->type == _LARGER_OR_EQUALS ||
+        state->current_token->type == _EQUALS ||
+        state->current_token->type == _DOT
+    ) {
+        tok = state->current_token;
+
+        if (tok->type == _OP_PLUS) {
+            parse_eat(state, _OP_PLUS);
+        } else if (tok->type == _OP_SUBTRACT) {
+            parse_eat(state, _OP_SUBTRACT);
+        } else if (tok->type == _NOT_EQUALS) {
+            parse_eat(state, _NOT_EQUALS);
+        } else if (tok->type == _LESS_THAN) {
+            parse_eat(state, _LESS_THAN);
+        } else if (tok->type == _LARGER_THAN) {
+            parse_eat(state, _LARGER_THAN);
+        } else if (tok->type == _LESS_OR_EQUALS) {
+            parse_eat(state, _LESS_OR_EQUALS);
+        } else if (tok->type == _LARGER_OR_EQUALS) {
+            parse_eat(state, _LARGER_OR_EQUALS);
+        } else if (tok->type == _EQUALS) {
+            parse_eat(state, _EQUALS);
+        } else if (tok->type == _DOT) {
+            parse_eat(state, _DOT);
+        }
+
+        // invalid pointer type
+        node = init_ast_node_binop(tok, node, parse_term(state));
+    };
+
+    return node;
+}
 
 ast_node* parse_parse() { return (void*)0; }
