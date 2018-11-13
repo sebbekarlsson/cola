02a0d4b1419eacebc56d25258ec3e79cf73c75b1
ianertson <sebbekarlsson97@gmail.com>
2018-11-13 20:46:35 +0100

    free memory

--- src/interpret.c
+++ src/interpret.c
@@ -86,6 +86,10 @@ ast_node* interpret_visit_integer(ast_node_integer* node) {
 
 ast_node* interpret_visit_component(ast_node_component* node) {
     printf("visit component %s \n", node->name->value);
+
+    if (node->deps->size == 0)
+        free(node->deps);
+
     interpret_visit((ast_node*) node->body);
     return (ast_node*) node;
 }
