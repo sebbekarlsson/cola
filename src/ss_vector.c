9101a52987414420db1a6bb5ed3550cfd96fcf32
ianertson <sebbekarlsson97@gmail.com>
2018-11-18 00:21:51 +0100

    remove blank lines

--- src/ss_vector.c
+++ src/ss_vector.c
@@ -10,13 +10,11 @@ ss_vector* ss_init_vector(size_t item_size) {
     return vector;
 }
 
-
 void ss_vector_append(ss_vector* vec, void* item) {
     vec->size++;
     vec->items = realloc(vec->items, vec->size * sizeof(item));
     vec->items[vec->size - 1] = item;
 };
-
 
 void ss_vector_free(ss_vector* vec) {
     for (int i = 0; i < vec->size; i++)
